560164782ea27716508dde5efdac4b647c222344
Nanda Eka <nandaekasuciramadan@gmail.com>
2022-06-18 13:24:24 +0700

    Updates

---
+++ precache-manifest.a938962c76ab4119a36260809839479b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d76cb2d9f64ea34038c6",
+    "url": "/testreactjs/static/css/main.6911dd06.chunk.css"
+  },
+  {
+    "revision": "d76cb2d9f64ea34038c6",
+    "url": "/testreactjs/static/js/main.d76cb2d9.chunk.js"
+  },
+  {
+    "revision": "e26974a781e160b516d0",
+    "url": "/testreactjs/static/js/1.e26974a7.chunk.js"
+  },
+  {
+    "revision": "e9b83b4b04583bb1869c",
+    "url": "/testreactjs/static/js/2.e9b83b4b.chunk.js"
+  },
+  {
+    "revision": "5395aeb34d0f2db2d7a5",
+    "url": "/testreactjs/static/js/runtime~main.5395aeb3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/testreactjs/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "36c58459144680bdddc063b1d49d3f8d",
+    "url": "/testreactjs/index.html"
+  }
+];
